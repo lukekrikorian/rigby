088114f34f9da84ee0cc6b268e3f08ceab248d7e
Luke Krikorian <lukekrikorian@gmail.com>
2019-01-04 23:24:00 -0500

    Conversation page profile link fix

---
+++ transfer.sh
@@ -0,0 +1,18 @@
+echo "Building go project..."
+go build
+echo "Built"
+echo "Killing website process..."
+ssh -t root@rigby.space 'fuser -k 443/tcp'
+echo "Process killed"
+echo "Transfering site file..."
+scp site root@rigby.space:~/rigby
+echo "File transfered"
+echo "Pulling changes from origin on remote machine..."
+ssh -t root@rigby.space 'cd ~/rigby && git pull'
+echo "Changes pulled"
+echo "Restarting server..."
+ssh -t root@rigby.space 'cd ~/rigby && ./site > server.log &'
+echo "Server restarted"
+echo "Testing site"
+sleep 2s
+curl https://rigby.space/~luke
