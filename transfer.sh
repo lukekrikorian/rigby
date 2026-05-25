cbf73dc13843ca44d8dba20775ff41c41cdeb7c3
Luke Krikorian <lukekrikorian@gmail.com>
2026-05-25 10:22:03 -0400

    delete transfer.sh

--- transfer.sh
+++
@@ -1,9 +0,0 @@
-echo "Building go project..."
-go build
-echo "Built"
-echo "Killing website process..."
-ssh -t root@rigby.space 'fuser -k 443/tcp'
-echo "Process killed"
-echo "Transfering site file..."
-scp site root@rigby.space:~/rigby
-echo "File transfered"
